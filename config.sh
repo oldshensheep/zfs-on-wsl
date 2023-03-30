2526698139bf9a3bf87fcd8d93b716c3b5802280
oldshensheep <oldshensheep@gmail.com>
2023-03-30 15:32:02 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export KERNELVER="5.15.93" # https://www.kernel.org/
+export KERNELVER="5.15.104" # https://www.kernel.org/
 export ZFSVER="2.1.9"       # https://zfsonlinux.org/
 export KERNELNAME="standard-with-zfs"
 export BUILD_DIR=$HOME # or $(pwd)
