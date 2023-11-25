48542a6f7c2c154f44c42ed5e74eb1e25a8954a4
Ruby Rose <oldshensheep@gmail.com>
2023-11-25 15:37:39 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 export KERNELVER="6.1.21.2"
 export WSL2_Linux_Kernel_BRANCH="linux-msft-wsl-6.1.y"
 
-export ZFSVER="2.2.0"       # https://zfsonlinux.org/
+export ZFSVER="2.2.1"       # https://zfsonlinux.org/
 
 export KERNELNAME="standard-with-zfs"
 
